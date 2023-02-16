3cf835353070035dd9eb67ec184c4eaec1fc57f4
Wilson The Ball <wilsontheball.gdev@gmail.com>
2023-02-16 15:11:32 +0400

    Interactable Objects
    Interactable Object Class and Example Scene

---
+++ Assets/InteractableObjects/Debug_PrintMaeesage_InteractableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.InteractableObjects
+{
+    public class Debug_PrintMaeesage_InteractableObject : MonoBehaviour
+    {
+        public void Print_PlayerEnter()
+        {
+            Debug.Log("Player Enter Interact zone");
+        }
+
+        public void Print_PlayerExit()
+        {
+            Debug.Log("Player Exit Interact zone");
+        }
+    }
+}
---
+++ Assets/InteractableObjects/InteractableObject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.InteractableObjects
+{
+    public class InteractableObject : MonoBehaviour
+    {
+        public UnityEvent PlayerEnterZone;
+        public UnityEvent PlayerExitZone;
+        public UnityEvent PlayerInteract;
+
+        private bool playerInRange;
+
+        private void Start()
+        {
+            SubscribeToPlayerEvents();
+        }
+
+
+
+        private void SubscribeToPlayerEvents()
+        {
+            
+        }
+
+        private void OnPlayerInteract()
+        {
+            if (playerInRange)
+            {
+                PlayerInteract.Invoke();
+            }
+        }
+
+        private bool IsPlayer(GameObject gameObject)
+        {
+            return gameObject.CompareTag("Player");
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (IsPlayer(collision.gameObject))
+            {
+                playerInRange = true;
+                PlayerEnterZone.Invoke();
+            }
+            
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                playerInRange = false;
+                PlayerExitZone.Invoke();
+            }
+               
+        }
+    }
+}
