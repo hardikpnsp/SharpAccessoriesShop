422249dd20288fe623644783c48c1e6d7c081d33
RubeoFay <125475301+RubeoFay@users.noreply.github.com>
2023-02-21 13:57:52 +0400

    Add BT package to the project

--- Assets/Customers/Scripts/Customer.cs
+++ Assets/Customers/Scripts/Customer.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NodeCanvas.BehaviourTrees;
 
-
+[RequireComponent(typeof(BehaviourTreeOwner))]
 public class Customer : MonoBehaviour
 {
+    private BehaviourTreeOwner _btOwner;
+    private Vector2 _spawnPoint;
+
+    private void Awake()
+    {
+        _btOwner = GetComponent<BehaviourTreeOwner>();
+        _btOwner.StopBehaviour();
+    }
+
     public void Spawn(Vector2 position)
     {
-        transform.position = position;
+        _spawnPoint = position;
+        transform.position = _spawnPoint;
+        _btOwner.StartBehaviour();
     }
 
     public void Despawn()
--- Assets/Customers/Scripts/CustomerRole.cs
+++ Assets/Customers/Scripts/CustomerRole.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NodeCanvas.BehaviourTrees;
 
 [CreateAssetMenu(fileName = "CustomerRole", menuName = "CustomerRole", order = 51)]
 public class CustomerRole : ScriptableObject
