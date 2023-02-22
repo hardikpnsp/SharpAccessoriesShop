9bf1dd6e94e8b83108599d5d47c2b3d080147014
Wilson The Ball <wilsontheball.gdev@gmail.com>
2023-02-22 21:45:08 +0400

    QueueController done

--- Assets/LevelMapObjects/QueueController.cs
+++ Assets/LevelMapObjects/QueueController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using Assets.InteractableObjects;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(BuyingItemsInteraction), typeof(InteractableObject))]
 public class QueueController : MonoBehaviour
 {
     public static QueueController Instance { get; private set; }
@@ -13,13 +15,24 @@ public class QueueController : MonoBehaviour
     [SerializeField]
     private List<Customer> customers = new List<Customer>();
 
+    public bool Empty
+    {
+        get
+        {
+            return this.customers.Count == 0;
+        }
+    }
+
     public CustomerServedEvent CustomerServed;
 
     [SerializeField]
-    private bool Debug_ImmidiatlyServCustomer;
+    private Transform startPos;
 
     [SerializeField]
-    private Transform startPos;
+    private Transform[] queuePositions;
+
+    [SerializeField]
+    private BuyingItemsInteraction interactable;
 
     private void Awake()
     {
@@ -27,20 +40,45 @@ public class QueueController : MonoBehaviour
         {
             Instance = this;
         }
+        else
+        {
+            Destroy(this);
+        }
+
+        if(interactable != null)
+        {
+            interactable.InteractionComplete.AddListener(OnPlayerInteract);
+        }
     }
 
-    public static Transform GetStartPosition()
+    public static Transform GetPosition(Customer customer)
     {
-        if(Instance != null)
-            return Instance.startPos;
-        else
-            return null;
+        if(Instance != null && customer != null)
+        {
+            if (Instance.customers.Contains(customer))
+            {
+                int index = Instance.customers.IndexOf(customer);
+
+                return Instance.queuePositions[index];
+            }
+            else
+            {
+                if (CanJoinQueue())
+                {
+                    return Instance.queuePositions[Instance.customers.Count + 1];
+                }
+            }
+        }
+        return null;
+
     }
 
     public static bool CanJoinQueue()
     {
         if (Instance != null)
-            return true;
+        {
+            return Instance.customers.Count < Instance.queuePositions.Length;
+        }
         else
             return false;
     }
@@ -53,37 +91,19 @@ public class QueueController : MonoBehaviour
         }
 
         Instance.customers.Add(customer);
-
-        if(Instance.Debug_ImmidiatlyServCustomer)
-        Instance.Service_Temp();
-        
-    }
-
-    private void Service_Temp()
-    {
-        if(customers.Count > 0)
-        {
-            Customer customer = customers[0];
-            customers.RemoveAt(0);
-
-            if (customer != null)
-            {
-                CustomerServed.Invoke(customer);
-            }
-        }
-       
         
     }
 
     public static void ExitQueue(Customer customer)
     {
-        if (Instance == null)
-        {
-            throw new System.Exception("QueueController Instance is null");
-        }
+        //if (Instance == null)
+        //{
+        //    throw new System.Exception("QueueController Instance is null");
+        //}
 
-        Instance.customers.Remove(customer);
+        //Instance.customers.Remove(customer);
     }
+
 
     private void OnDestroy()
     {
@@ -91,6 +111,14 @@ public class QueueController : MonoBehaviour
         {
             Instance = null;
         }
+    }
+
+    private void OnPlayerInteract(Interaction.InteractionResult result)
+    {
+        customers.Remove(customers[0]);
+
+        CustomerServed.Invoke(customers[0]);
+
     }
 
     [System.Serializable]
